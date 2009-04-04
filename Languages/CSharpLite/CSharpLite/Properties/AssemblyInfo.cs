c9eb21c25b8829f56c8df2492135c6e1ce4c0cd5
hermanv_cp <SND\hermanv_cp>
2009-04-03 19:24:44 -0700

    Use the common directory of the metadata project for version information.

--- Languages/CSharpLite/CSharpLite/Properties/AssemblyInfo.cs
+++ Languages/CSharpLite/CSharpLite/Properties/AssemblyInfo.cs
@@ -13,9 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CSharpLite")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("CSharpLite")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2006")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,14 +25,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bbcf22c1-a561-47f4-97aa-9e02ebb71543")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
