f99ba71a43db3a748c1cf0e2d5aa9f0de5b0f2a0
pelikhan_cp <SND\pelikhan_cp>
2009-04-09 16:55:51 -0700

    making the test project self-runnable

---
+++ Tests/RoundtripTests/Program.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoundtripTests
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var newArgs = new List<string>();
+            newArgs.Add(typeof(Program).Assembly.Location);
+            if (args != null)
+                newArgs.AddRange(args);
+            return Xunit.ConsoleClient.Program.Main(newArgs.ToArray());
+        }
+    }
+}
