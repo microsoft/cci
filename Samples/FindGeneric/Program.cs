a93ed0a2b289613ea18fc1860bf7eb8c2fb0c13f
MatkoBotincan_cp <SND\MatkoBotincan_cp>
2009-04-14 15:01:55 -0700

    A metadata sample that finds generic methods within generic types inside an assembly.

---
+++ Samples/FindGeneric/Program.cs
@@ -0,0 +1,96 @@
+﻿//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+using System;
+using System.IO;
+using Microsoft.Cci;
+
+namespace FindGeneric
+{
+  class Program
+  {
+    #region snippet FindGeneric-Program-Main
+    static int Main(string[] args)
+    {
+      #region remove
+      if (args.Length < 1)
+      {
+        Console.WriteLine("Must specify at least one input file.");
+        return 1;
+      }
+      #endregion
+      #region snippet FindGeneric-Program-Main-HostEnvironment
+      HostEnvironment host = new HostEnvironment();
+      #endregion
+
+      foreach (string assemblyName in args)
+      {
+        #region snippet FindGeneric-Program-Main-LoadUnitFrom
+        IAssembly/*?*/ assembly = host.LoadUnitFrom(assemblyName)
+          as IAssembly;
+        #endregion
+        if (assembly == null || assembly == Dummy.Assembly)
+        {
+          #region remove
+          Console.WriteLine("The file '" + assemblyName + "' is not a PE file" +
+            " containing a CLR assembly, or an error occurred when loading it.");
+          #endregion
+          continue;
+        }
+        else
+        {
+          Console.WriteLine("Generic Methods in generic types from '"
+            + assembly.Name.Value + "':");
+          #region remove
+          Console.WriteLine("=====================================");
+          #endregion
+        }
+
+        #region snippet FindGeneric-Program-Main-ForLoop
+        foreach (INamedTypeDefinition type in assembly.GetAllTypes())
+        {
+          if (type.IsGeneric)
+          {
+            foreach (IMethodDefinition methodDefinition in type.Methods)
+            {
+              if (methodDefinition.IsGeneric)
+              {
+                Console.WriteLine(MemberHelper.GetMemberSignature(
+                  methodDefinition,
+                  NameFormattingOptions.Signature |
+                  NameFormattingOptions.TypeParameters |
+                  NameFormattingOptions.TypeConstraints |
+                  NameFormattingOptions.ParameterName
+                  ));
+              }
+            }
+          }
+        }
+        #endregion
+      }
+      return 0;
+    }
+    #endregion
+  }
+
+  #region snippet FindGeneric-HostEnvironment
+  internal class HostEnvironment : MetadataReaderHost
+  {
+    PeReader peReader;
+    internal HostEnvironment()
+      : base(new NameTable(), 4)
+    {
+      this.peReader = new PeReader(this);
+    }
+    public override IUnit LoadUnitFrom(string location)
+    {
+      IUnit result = this.peReader.OpenModule(
+        BinaryDocument.GetBinaryDocumentForFile(location, this));
+      this.RegisterAsLatest(result);
+      return result;
+    }
+  }
+  #endregion
+}
---
+++ Samples/FindGeneric/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FindGeneric")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FindGeneric")]
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("abef6852-f5ba-4586-ab23-74502e03189d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
