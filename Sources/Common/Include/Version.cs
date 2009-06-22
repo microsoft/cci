3c4087b27fd9a7b3ed80c8c6a0ccfe49ca5c7ae9
pelikhan_cp <SND\pelikhan_cp>
2009-06-22 09:40:53 -0700

    replace source link to xunit to checked in binaries

--- Sources/Common/Include/Version.cs
+++ Sources/Common/Include/Version.cs
@@ -1,14 +1,13 @@
 // ==++==
-// 
+//
 //   Copyright (c) Microsoft Corporation.  All rights reserved.
-// 
+//
 // ==--==
 // Warning: Automatically generated file. DO NOT EDIT
-// Generated at Tuesday, October 09, 2007 9:54:09 AM
+// Generated at 
 
 using System.Reflection;
-[assembly: AssemblyVersion("0.0.0.2")]
-[assembly: AssemblyFileVersion("0.0.0.2")]
+[assembly: AssemblyVersion("2.0.8.17659")]
+[assembly: AssemblyFileVersion("2.0.8.17659")]
 [assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation. All rights reserved.")]
-
