eaf3978a8840d53126ef522c41ba62b388671be4
hermanv_cp <SND\hermanv_cp>
2009-12-14 19:54:34 -0800

    Clean up FindGeneric sample.

--- Samples/FindGeneric/Program.cs
+++ Samples/FindGeneric/Program.cs
@@ -4,93 +4,36 @@
 //
 //-----------------------------------------------------------------------------
 using System;
-using System.IO;
 using Microsoft.Cci;
 
-namespace FindGeneric
-{
-  class Program
-  {
-    #region snippet FindGeneric-Program-Main
-    static int Main(string[] args)
-    {
-      #region remove
-      if (args.Length < 1)
-      {
+namespace FindGeneric {
+  class Program {
+    static void Main(string[] args) {
+      if (args.Length < 1) {
         Console.WriteLine("Must specify at least one input file.");
-        return 1;
+        return;
       }
-      #endregion
-      #region snippet FindGeneric-Program-Main-HostEnvironment
-      HostEnvironment host = new HostEnvironment();
-      #endregion
+      MetadataReaderHost host = new PeReader.DefaultHost();
 
-      foreach (string assemblyName in args)
-      {
-        #region snippet FindGeneric-Program-Main-LoadUnitFrom
-        IAssembly/*?*/ assembly = host.LoadUnitFrom(assemblyName)
-          as IAssembly;
-        #endregion
-        if (assembly == null || assembly == Dummy.Assembly)
-        {
-          #region remove
+      foreach (string assemblyName in args) {
+        var assembly = host.LoadUnitFrom(assemblyName) as IAssembly;
+        if (assembly == null || assembly == Dummy.Assembly) {
           Console.WriteLine("The file '" + assemblyName + "' is not a PE file" +
             " containing a CLR assembly, or an error occurred when loading it.");
-          #endregion
           continue;
         }
-        else
-        {
-          Console.WriteLine("Generic Methods in generic types from '"
-            + assembly.Name.Value + "':");
-          #region remove
-          Console.WriteLine("=====================================");
-          #endregion
-        }
 
-        #region snippet FindGeneric-Program-Main-ForLoop
-        foreach (INamedTypeDefinition type in assembly.GetAllTypes())
-        {
-          if (type.IsGeneric)
-          {
-            foreach (IMethodDefinition methodDefinition in type.Methods)
-            {
-              if (methodDefinition.IsGeneric)
-              {
-                Console.WriteLine(MemberHelper.GetMemberSignature(
-                  methodDefinition,
-                  NameFormattingOptions.Signature |
-                  NameFormattingOptions.TypeParameters |
-                  NameFormattingOptions.TypeConstraints |
-                  NameFormattingOptions.ParameterName
-                  ));
-              }
+        Console.WriteLine("Generic Methods in generic types from '"+assembly.Name.Value+"':");
+        foreach (INamedTypeDefinition type in assembly.GetAllTypes()) {
+          if (!type.IsGeneric) continue;
+          foreach (IMethodDefinition methodDefinition in type.Methods) {
+            if (methodDefinition.IsGeneric) {
+              Console.WriteLine(MemberHelper.GetMemberSignature(methodDefinition,
+                NameFormattingOptions.Signature | NameFormattingOptions.TypeParameters | NameFormattingOptions.TypeConstraints | NameFormattingOptions.ParameterName));
             }
           }
         }
-        #endregion
       }
-      return 0;
-    }
-    #endregion
-  }
-
-  #region snippet FindGeneric-HostEnvironment
-  internal class HostEnvironment : MetadataReaderHost
-  {
-    PeReader peReader;
-    internal HostEnvironment()
-      : base(new NameTable(), 4)
-    {
-      this.peReader = new PeReader(this);
-    }
-    public override IUnit LoadUnitFrom(string location)
-    {
-      IUnit result = this.peReader.OpenModule(
-        BinaryDocument.GetBinaryDocumentForFile(location, this));
-      this.RegisterAsLatest(result);
-      return result;
     }
   }
-  #endregion
 }
