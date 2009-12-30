b12ef21935566455147bcb00f7b98eb2369df030
pelikhan_cp <SND\pelikhan_cp>
2009-12-29 21:08:33 -0800

    start dependency injection mutator

---
+++ Applications/CciSharp/CciSharp.Test/DependencyAutoPropertyTest.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------------
+//
+// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+//
+//-----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.ComponentModel;
+using Xunit;
+
+namespace CciSharp.Test
+{
+    public partial class DependencyAutoPropertyTest
+    {
+        [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+        public class DependencyPropertyAttribute : Attribute { }
+
+        class Foo : DependencyObject
+        {
+            [DependencyProperty]
+            public int Value { get; set; }
+            [DependencyProperty, DefaultValue(10)]
+            public int ValueWithDefault { get; set; }
+        }
+
+        [Fact(Skip = "not ready")]
+        public void ValueTest()
+        {
+            var foo = new Foo();
+        }
+    }
+}
---
+++ Applications/CciSharp/DependencyAutoProperty/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using CciSharp.Framework;
+using Microsoft.Cci.MutableCodeModel;
+using Microsoft.Cci;
+
+namespace CciSharp.Mutators
+{
+    /// <summary>
+    /// A mutator that instruments an autoproperty to implement the DependencyProperty pattern.
+    /// </summary>
+    public sealed class DependencyAutoProperty
+        : CcsMutatorBase
+    {
+        public DependencyAutoProperty(ICcsHost host)
+            :base(host, "Dependency Auto Property", 20, typeof(DependencyAutoPropertyResources))
+        {
+        }
+
+        public override bool Visit(Assembly assembly, PdbReader pdbReader)
+        {
+            return false;
+        }
+    }
+}
