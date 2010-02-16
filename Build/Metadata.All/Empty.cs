290a75de17c1689e4fb973ed939db166df498bc8
hermanv_cp <SND\hermanv_cp>
2010-02-16 13:33:07 -0800

    Update copyright notice.

--- Build/Metadata.All/Empty.cs
+++ Build/Metadata.All/Empty.cs
@@ -1,6 +1,11 @@
 ﻿//-----------------------------------------------------------------------------
 //
-// Copyright (C) Microsoft Corporation.  All Rights Reserved.
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.
+// This code is licensed under the Microsoft Public License.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
 //
 //-----------------------------------------------------------------------------
 
--- Sources/Common/Include/Version.cs
+++ Sources/Common/Include/Version.cs
@@ -1,6 +1,11 @@
 // ==++==
 //
-//   Copyright (c) Microsoft Corporation.  All rights reserved.
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.
+// This code is licensed under the Microsoft Public License.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
 //
 // ==--==
 // Warning: Automatically generated file. DO NOT EDIT
