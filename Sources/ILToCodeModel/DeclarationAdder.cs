a4ffbf7fd71b8262623ea2e4cd8c5621602a9818
hermanv_cp <SND\hermanv_cp>
2010-06-13 21:05:43 -0700

    Do not unify local variable declarations with the first top level assignment to the local, unless it is impossible for any code path to reference the variable before that assignment has been reached.

--- Sources/ILToCodeModel/DeclarationAdder.cs
+++ Sources/ILToCodeModel/DeclarationAdder.cs
@@ -1,6 +1,6 @@
 ﻿//-----------------------------------------------------------------------------
 //
-// Copyright (c) Microsoft Corporation.  All Rights Reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // This code is licensed under the Microsoft Public License.
 // THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
 // ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
@@ -13,63 +13,78 @@ using Microsoft.Cci.MutableCodeModel;
 
 namespace Microsoft.Cci.ILToCodeModel {
 
+  /// <summary>
+  /// Add LocalDeclarationStatement instances to the appropriate blocks.
+  /// </summary>
+  /// <remarks>
+  /// The metadata model does not have the concept of block scopes. All local variables used in a method body are listed in the LocalVariables property of IMethodBody.
+  /// The CodeModel on the other hand, has explicit blocks and these blocks contain explicit LocalDeclarationStatement instances that declare the variables that are local
+  /// to a block. This visitor decompiles the MetaData model to the CodeModel by inserting (adding) the necessary LocalDeclarationStatement instances in the 
+  /// appropriate places. 
+  /// </remarks>
   internal class DeclarationAdder : BaseCodeTraverser {
 
     Dictionary<ILocalDefinition, bool> declaredLocals = new Dictionary<ILocalDefinition, bool>();
 
     internal void Visit(SourceMethodBody body, BasicBlock rootBlock) {
       this.Visit(rootBlock);
+      //Now add declarations for any locals declared only on the body (for example temporary variables introduced by Unstacker).
       List<IStatement> prelude = new List<IStatement>();
-      // For the first assignment to a local variable in the rootblock before a control statement is hit,
-      // if the local variable is not mentioned previously, we turn this assignment into a local declaration. 
-      List<ILocalDefinition> topLevelLocals = new List<ILocalDefinition>(body.LocalVariables);
-      List<ILocalDefinition> localsMet = new List<ILocalDefinition>();
-      for (int i = 0; i < rootBlock.Statements.Count; i++) {
-        if (topLevelLocals.Count ==0) break;
-        IExpressionStatement expressionStatement = rootBlock.Statements[i] as IExpressionStatement;
-        if (expressionStatement != null) {
-          IAssignment assignment = expressionStatement.Expression as IAssignment;
-          if (assignment != null) {
-            ILocalDefinition localDef = assignment.Target.Definition as ILocalDefinition;
-            if (localDef != null && topLevelLocals.Contains(localDef) && !localsMet.Contains(localDef)) {
-              LocalDeclarationStatement localDecl = new LocalDeclarationStatement() {
-                LocalVariable = localDef, InitialValue = assignment.Source, 
-              };
-              rootBlock.Statements[i] = localDecl;
-              topLevelLocals.Remove(localDef);
-              localsMet.Add(localDef);
-            }
-          }
-        }
-        LocalFinder finder = new LocalFinder();
-        finder.Visit(rootBlock.Statements[i]);
-        foreach (ILocalDefinition local in finder.FoundLocals) {
-          if (!localsMet.Contains(local)) localsMet.Add(local);
-        }
-        IGotoStatement gotoStatement = rootBlock.Statements[i] as IGotoStatement;
-        if (gotoStatement != null) break;
-        IConditionalStatement conditionalStatement = rootBlock.Statements[i] as IConditionalStatement;
-        if (conditionalStatement != null) break;
-        ISwitchStatement switchStatement = rootBlock.Statements[i] as ISwitchStatement;
-        if (switchStatement != null) break;
-        IForEachStatement foreachStatement = rootBlock.Statements[i] as IForEachStatement;
-        if (foreachStatement != null) break;
-        IForStatement forStatement = rootBlock.Statements[i] as IForStatement;
-        if (forStatement != null) break;
-        ITryCatchFinallyStatement tryStatement = rootBlock.Statements[i] as ITryCatchFinallyStatement;
-        if (tryStatement != null) break;
-      }
-      // For the remaining locals in the body, generate local declarations. 
-      foreach (var localDef in topLevelLocals) {
+      this.AddDeclarationsWithInitialValues(body.LocalVariables, rootBlock);
+      foreach (var localDef in body.LocalVariables) {
         if (this.declaredLocals.ContainsKey(localDef)) continue;
-        int numRefs;
-        if (!body.numberOfReferences.TryGetValue(localDef, out numRefs) || numRefs == 0) continue;
         LocalDeclarationStatement localDecl = new LocalDeclarationStatement();
         localDecl.LocalVariable = localDef;
         prelude.Add(localDecl);
       }
       if (prelude.Count > 0)
         rootBlock.Statements.InsertRange(0, prelude); //TODO: use pdb info to insert them in the same order they appear in the source
+    }
+
+    // For the first assignment to a local variable in a block before a control statement is hit,
+    // if the local variable is not mentioned previously, we turn this assignment into a local declaration. 
+    private void AddDeclarationsWithInitialValues(IEnumerable<ILocalDefinition> localVariables, BasicBlock block) {
+      List<ILocalDefinition> topLevelLocals = new List<ILocalDefinition>(localVariables);
+      List<ILocalDefinition> localsMet = new List<ILocalDefinition>();
+      for (int i = 0; i < block.Statements.Count; i++) {
+        if (topLevelLocals.Count == 0) break;
+        IExpressionStatement expressionStatement = block.Statements[i] as IExpressionStatement;
+        if (expressionStatement != null) {
+          IAssignment assignment = expressionStatement.Expression as IAssignment;
+          if (assignment != null) {
+            ILocalDefinition localDef = assignment.Target.Definition as ILocalDefinition;
+            if (localDef != null && topLevelLocals.Contains(localDef) && !localsMet.Contains(localDef) && !this.declaredLocals.ContainsKey(localDef)) {
+              LocalDeclarationStatement localDecl = new LocalDeclarationStatement() {
+                LocalVariable = localDef, InitialValue = assignment.Source,
+              };
+              this.declaredLocals.Add(localDef, true);
+              block.Statements[i] = localDecl;
+              topLevelLocals.Remove(localDef);
+              localsMet.Add(localDef);
+            }
+          }
+        }
+        LocalFinder finder = new LocalFinder();
+        finder.Visit(block.Statements[i]);
+        foreach (ILocalDefinition local in finder.FoundLocals) {
+          if (!localsMet.Contains(local)) localsMet.Add(local);
+        }
+        //Once we see a statement that can transfer control somewhere else, we
+        //no longer know that any subsequent assignment dominates all references
+        //and hence cannot postpone adding the declaration until we can unify it with the assignment.
+        IGotoStatement gotoStatement = block.Statements[i] as IGotoStatement;
+        if (gotoStatement != null) break;
+        IConditionalStatement conditionalStatement = block.Statements[i] as IConditionalStatement;
+        if (conditionalStatement != null) break;
+        ISwitchStatement switchStatement = block.Statements[i] as ISwitchStatement;
+        if (switchStatement != null) break;
+        IForEachStatement foreachStatement = block.Statements[i] as IForEachStatement;
+        if (foreachStatement != null) break;
+        IForStatement forStatement = block.Statements[i] as IForStatement;
+        if (forStatement != null) break;
+        ITryCatchFinallyStatement tryStatement = block.Statements[i] as ITryCatchFinallyStatement;
+        if (tryStatement != null) break;
+      }
     }
 
     class LocalFinder : BaseCodeTraverser {
@@ -89,36 +104,16 @@ namespace Microsoft.Cci.ILToCodeModel {
       BasicBlock basicBlock = (BasicBlock)block;
       List<ILocalDefinition>/*?*/ localsInCurrentScope = basicBlock.LocalVariables;
       if (localsInCurrentScope != null) {
-        localsInCurrentScope = new List<ILocalDefinition>(localsInCurrentScope);
-        for (int i = 0, n = basicBlock.Statements.Count; i < n; i++) {
-          ExpressionStatement/*?*/ expressionStatement = basicBlock.Statements[i] as ExpressionStatement;
-          if (expressionStatement == null) continue;
-          Assignment/*?*/ assignment = expressionStatement.Expression as Assignment;
-          if (assignment == null) continue;
-          ILocalDefinition/*?*/ localDef = assignment.Target.Definition as ILocalDefinition;
-          if (localDef == null) continue;
-          if (localsInCurrentScope.Contains(localDef)) {
-            LocalDeclarationStatement localDecl = new LocalDeclarationStatement();
-            localDecl.LocalVariable = localDef;
-            localDecl.InitialValue = assignment.Source;
-            localDecl.Locations = expressionStatement.Locations;
-            if (localDef.Type == Dummy.TypeReference && localDef is LocalDefinition) {
-              ((LocalDefinition)localDef).Type = assignment.Source.Type;
-            }
-            basicBlock.Statements[i] = localDecl;
-            localsInCurrentScope.Remove(localDef);
-            this.declaredLocals[localDef] = true;
-          }
+        this.AddDeclarationsWithInitialValues(localsInCurrentScope, basicBlock);
+        List<IStatement> prelude = new List<IStatement>(localsInCurrentScope.Count);
+        foreach (ILocalDefinition localDef in localsInCurrentScope) {
+          if (this.declaredLocals.ContainsKey(localDef)) continue;
+          LocalDeclarationStatement localDecl = new LocalDeclarationStatement();
+          localDecl.LocalVariable = localDef;
+          prelude.Add(localDecl);
         }
-        if (localsInCurrentScope.Count > 0) {
-          List<IStatement> prelude = new List<IStatement>(localsInCurrentScope.Count);
-          foreach (ILocalDefinition localDef in localsInCurrentScope) {
-            LocalDeclarationStatement localDecl = new LocalDeclarationStatement();
-            localDecl.LocalVariable = localDef;
-            prelude.Add(localDecl);
-          }
+        if (prelude.Count > 0)
           basicBlock.Statements.InsertRange(0, prelude); //TODO: use pdb info to insert them in the same order they appear in the source
-        }
       }
       this.Visit(basicBlock.Statements);
     }
