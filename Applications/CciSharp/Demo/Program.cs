da6b21bff9de5062f4374053b261089d058b7608
pelikhan_cp <SND\pelikhan_cp>
2010-07-12 08:48:12 -0700

    added demo

---
+++ Applications/CciSharp/Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demo {
+  public class Program {
+    public static string NeverReturnsNull() {
+      // the following post condition should be automatically injected:
+      // Contract.Ensures(Contract.Result<string>() != null);
+      return null;
+    }
+
+    static void Main(string[] args) {
+      Console.WriteLine(NeverReturnsNull());
+    }
+  }
+}
