ae7b2f8c00b97f15b3fa3cc84540271bd817a616
pelikhan_cp <SND\pelikhan_cp>
2010-07-12 08:57:58 -0700

    updated sources

--- Applications/CciSharp/EnsuresNotNull/EnsuresNotNullMutator.cs
+++ Applications/CciSharp/EnsuresNotNull/EnsuresNotNullMutator.cs
@@ -7,74 +7,80 @@ using Microsoft.Cci.Contracts;
 using Microsoft.Cci.MutableCodeModel;
 using Microsoft.Cci.MutableContracts;
 
-namespace CciSharp.Mutators
-{
-  public class EnsuresNotNullMutator : CcsMutatorBase 
-  {
-    public EnsuresNotNullMutator(ICcsHost host)
-      : base(host, "Ensures Not Null", 0, typeof(EnsuresNotNullMutator))
-        {
-        }
+namespace CciSharp.Mutators {
+  /// <summary>
+  /// A mutator that injects non-null postconditions
+  /// in visible methods
+  /// </summary>
+  public sealed class EnsuresNotNullMutator
+    : CcsMutatorBase {
 
+    public EnsuresNotNullMutator(ICcsHost host)
+      : base(host,"Ensures Not Null",0,typeof(EnsuresNotNullMutator)) {
+    }
 
     public override bool Visit() {
       var assembly = this.Host.MutatedAssembly;
       PdbReader _pdbReader;
-      if (!this.Host.TryGetMutatedPdbReader(out _pdbReader))
+      if(!this.Host.TryGetMutatedPdbReader(out _pdbReader))
         _pdbReader = null;
       var contracts = this.Host.MutatedContracts;
 
-      new NonNullInjector(this.Host, contracts).Visit(this.Host.MutatedAssembly);
-      ContractHelper.InjectContractCalls(this.Host, this.Host.MutatedAssembly, contracts, _pdbReader);
+      new NonNullInjector(this.Host,contracts).Visit(this.Host.MutatedAssembly);
+      ContractHelper.InjectContractCalls(this.Host,this.Host.MutatedAssembly,contracts,_pdbReader);
 
       return true;
     }
-  }
-  class NonNullInjector : BaseCodeTraverser {
 
-    IMetadataHost host;
-    Microsoft.Cci.MutableContracts.ContractProvider contractProvider;
+    sealed class NonNullInjector
+      : BaseCodeTraverser {
+      readonly IMetadataHost host;
+      readonly ContractProvider contractProvider;
 
-    public NonNullInjector(
-      IMetadataHost host,
-      Microsoft.Cci.MutableContracts.ContractProvider contractProvider) {
-      this.host = host;
-      this.contractProvider = contractProvider;
-    }
-
-    public override void Visit(IMethodDefinition method) {
-      if (!MemberHelper.IsVisibleOutsideAssembly(method)) return;
-      var returnType = method.Type;
-      if (returnType == this.host.PlatformType.SystemVoid
-        || returnType.IsEnum
-        || returnType.IsValueType
-        ) return;
-
-      var newContract = new Microsoft.Cci.MutableContracts.MethodContract();
-      var post = new List<IPostcondition>();
-      var p = new Microsoft.Cci.MutableContracts.PostCondition() {
-        Condition = new NotEquality() {
-          LeftOperand = new ReturnValue() { Type = returnType, },
-          RightOperand = new CompileTimeConstant() {
-            Type = returnType,
-            Value = null,
-          },
-          Type = this.host.PlatformType.SystemBoolean,
-        },
-        OriginalSource = "result != null",
-      };
-      post.Add(p);
-      newContract.Postconditions = post;
-
-      var contract = this.contractProvider.GetMethodContractFor(method);
-      if (contract != null) {
-        Microsoft.Cci.MutableContracts.ContractHelper.AddMethodContract(newContract, contract);
+      public NonNullInjector(
+        IMetadataHost host,
+        ContractProvider contractProvider) {
+        this.host = host;
+        this.contractProvider = contractProvider;
       }
-      this.contractProvider.AssociateMethodWithContract(method, newContract);
 
-      base.Visit(method);
+      public override void Visit(IMethodDefinition method) {
+        // inject only in visible method
+        if(!MemberHelper.IsVisibleOutsideAssembly(method))
+          return;
+
+        // inject only in methods that return a reference type
+        var returnType = method.Type;
+        if(returnType == this.host.PlatformType.SystemVoid
+          || returnType.IsValueType
+          )
+          return;
+
+        // create new postcondition
+        // Contract.Ensures(Contract.Result<T>() != null);
+        var newContract = new MethodContract {
+          Postconditions = new List<IPostcondition> {
+            new PostCondition {
+              Condition = new NotEquality {
+                LeftOperand = new ReturnValue { Type = returnType, },
+                RightOperand = new CompileTimeConstant {
+                  Type = returnType,
+                  Value = null,
+                },
+                Type = this.host.PlatformType.SystemBoolean,
+              },
+              OriginalSource = "result != null",
+            }
+          }
+        };
+
+        // merge existing contracts, if any
+        var contract = this.contractProvider.GetMethodContractFor(method);
+        if(contract != null) {
+          ContractHelper.AddMethodContract(newContract,contract);
+        }
+        this.contractProvider.AssociateMethodWithContract(method,newContract);
+      }
     }
   }
-
-
 }
