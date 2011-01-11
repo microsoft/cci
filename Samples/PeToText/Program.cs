35432fe687d8dd704637dc5d6a92f278d31d72d2
hermanv_cp <SND\hermanv_cp>
2011-01-11 11:10:49 -0800

    Add a PeToText sample.

---
+++ Samples/PeToText/Program.cs
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.
+// This code is licensed under the Microsoft Public License.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//-----------------------------------------------------------------------------
+using System;
+using System.IO;
+using Microsoft.Cci;
+
+namespace PeToText {
+  class Program {
+    static void Main(string[] args) {
+      if (args == null || args.Length == 0) {
+        Console.WriteLine("usage: peToText [path]fileName.ext");
+        return;
+      }
+      using (var host = new PeReader.DefaultHost()) {
+        IModule/*?*/ module = host.LoadUnitFrom(args[0]) as IModule;
+        if (module == null || module == Dummy.Module || module == Dummy.Assembly) {
+          Console.WriteLine(args[0] + " is not a PE file containing a CLR module or assembly.");
+          return;
+        }
+
+        PdbReader/*?*/ pdbReader = null;
+        string pdbFile = Path.ChangeExtension(module.Location, "pdb");
+        if (File.Exists(pdbFile)) {
+          Stream pdbStream = File.OpenRead(pdbFile);
+          pdbReader = new PdbReader(pdbStream, host);
+        }
+        using (pdbReader) {
+          string txtFile = Path.ChangeExtension(pdbFile, "txt");
+          var writer = new StreamWriter(txtFile);
+          SourceEmitter csSourceEmitter = new SourceEmitter(writer, host, pdbReader);
+          csSourceEmitter.Visit((INamespaceDefinition)module.UnitNamespaceRoot);
+          writer.Close();
+        }
+      }
+    }
+  }
+
+}
---
+++ Samples/PeToText/SourceEmitter.cs
@@ -0,0 +1,137 @@
+﻿//-----------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.
+// This code is licensed under the Microsoft Public License.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//-----------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Cci;
+using Microsoft.Cci.MetadataReader;
+
+namespace PeToText {
+  public class SourceEmitter : BaseMetadataTraverser {
+
+    public SourceEmitter(TextWriter writer, IMetadataHost host, PdbReader/*?*/ pdbReader) {
+      this.writer = writer;
+      this.host = host;
+      this.pdbReader = pdbReader;
+    }
+
+    TextWriter writer;
+    IMetadataHost host;
+    PdbReader/*?*/ pdbReader;
+
+    public override void Visit(IMethodDefinition method) {
+      this.writer.WriteLine(MemberHelper.GetMethodSignature(method, NameFormattingOptions.Signature));
+      this.writer.WriteLine();
+      if (!method.IsAbstract && !method.IsExternal)
+        this.Visit(method.Body);
+      this.writer.WriteLine();
+      this.writer.WriteLine("*******************************************************************************");
+      this.writer.WriteLine();
+    }
+
+    public override void Visit(IMethodBody methodBody) {
+      if (this.pdbReader != null)
+        this.PrintScopes(methodBody);
+      else
+        this.PrintLocals(methodBody.LocalVariables);
+
+      int currentIndex = -1; // a number no index matches
+      foreach (IOperation operation in methodBody.Operations) {
+        if (this.pdbReader != null) {
+          foreach (IPrimarySourceLocation psloc in this.pdbReader.GetPrimarySourceLocationsFor(operation.Location)) {
+            if (psloc.StartIndex != currentIndex) {
+              this.PrintSourceLocation(psloc);
+              currentIndex = psloc.StartIndex;
+            }
+          }
+        }
+        this.PrintOperation(operation);
+      }
+    }
+
+    private void PrintScopes(IMethodBody methodBody) {
+      foreach (ILocalScope scope in this.pdbReader.GetLocalScopes(methodBody))
+        this.PrintScopes(scope);
+    }
+
+    private void PrintScopes(ILocalScope scope) {
+      this.writer.Write(string.Format("IL_{0} ... IL_{1} ", scope.Offset.ToString("x4"), scope.Length.ToString("x4")), true);
+      this.writer.WriteLine("{");
+      this.PrintConstants(this.pdbReader.GetConstantsInScope(scope));
+      this.PrintLocals(this.pdbReader.GetVariablesInScope(scope));
+      this.writer.WriteLine("}");
+    }
+
+    private void PrintConstants(IEnumerable<ILocalDefinition> locals) {
+      foreach (ILocalDefinition local in locals) {
+        this.writer.Write("  const ", true);
+        this.PrintTypeReference(local.Type);
+        this.writer.WriteLine(" "+this.GetLocalName(local)+";");
+      }
+    }
+
+    private void PrintTypeReference(ITypeReference typeReference) {
+      this.writer.Write(TypeHelper.GetTypeName(typeReference));
+    }
+
+    private void PrintLocals(IEnumerable<ILocalDefinition> locals) {
+      foreach (ILocalDefinition local in locals) {
+        this.writer.Write("  ");
+        this.PrintTypeReference(local.Type);
+        this.writer.WriteLine(" "+this.GetLocalName(local)+";");
+      }
+    }
+
+    private void PrintLocalName(ILocalDefinition local) {
+      this.writer.Write(this.GetLocalName(local));
+    }
+
+    private void PrintOperation(IOperation operation) {
+      this.writer.Write("IL_" + operation.Offset.ToString("x4") + ": ", true);
+      this.writer.Write(operation.OperationCode.ToString());
+      ILocalDefinition/*?*/ local = operation.Value as ILocalDefinition;
+      if (local != null)
+        this.writer.Write(" "+this.GetLocalName(local));
+      else if (operation.Value is string)
+        this.writer.Write(" \""+operation.Value+"\"");
+      else if (operation.Value != null) {
+        if (OperationCode.Br_S <= operation.OperationCode && operation.OperationCode <= OperationCode.Blt_Un)
+          this.writer.Write(" IL_"+((uint)operation.Value).ToString("x4"));
+        else if (operation.OperationCode == OperationCode.Switch) {
+          foreach (uint i in (uint[])operation.Value)
+            this.writer.Write(" IL_"+i.ToString("x4"));
+        } else
+          this.writer.Write(" "+operation.Value);
+      }
+      this.writer.WriteLine("", false);
+    }
+
+    protected virtual string GetLocalName(ILocalDefinition local) {
+      string localName = local.Name.Value;
+      if (this.pdbReader != null) {
+        foreach (IPrimarySourceLocation psloc in this.pdbReader.GetPrimarySourceLocationsForDefinitionOf(local)) {
+          if (psloc.Source.Length > 0) {
+            localName = psloc.Source;
+            break;
+          }
+        }
+      }
+      return localName;
+    }
+
+    private void PrintSourceLocation(IPrimarySourceLocation psloc) {
+      this.writer.WriteLine("");
+      this.writer.Write(psloc.Document.Name.Value+"("+psloc.StartLine+":"+psloc.StartColumn+")-("+psloc.EndLine+":"+psloc.EndColumn+"): ", true);
+      this.writer.WriteLine(psloc.Source);
+    }
+  }
+
+}
