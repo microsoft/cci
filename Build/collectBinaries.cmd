c2984940ff6dad0fb89e453f1b5b38c3f9d0d284
stobies_cp <SND\stobies_cp>
2011-12-09 01:35:00 -0800

    collect binaries from Release

--- Build/collectBinaries.cmd
+++ Build/collectBinaries.cmd
@@ -2,10 +2,10 @@
 if exist ..\Binaries ( rd /S /Q ..\Binaries )
 md ..\Binaries
 
-copy ..\Sources\AstsProjectedAsCodeModel\bin\Debug\*.dll ..\Binaries
-copy ..\Sources\AstsProjectedAsCodeModel\bin\Debug\*.pdb ..\Binaries
-copy ..\Sources\AstsProjectedAsCodeModel\bin\Debug\*.xml ..\Binaries
-copy ..\Metadata\Sources\PeReader\bin\Debug\Microsoft.Cci.PeReader.dll ..\Binaries
-copy ..\Metadata\Sources\PeReader\bin\Debug\Microsoft.Cci.PeReader.pdb ..\Binaries
-copy ..\Metadata\Sources\PeReader\bin\Debug\Microsoft.Cci.PeReader.xml ..\Binaries
+copy ..\Sources\AstsProjectedAsCodeModel\bin\Release\*.dll ..\Binaries
+copy ..\Sources\AstsProjectedAsCodeModel\bin\Release\*.pdb ..\Binaries
+copy ..\Sources\AstsProjectedAsCodeModel\bin\Release\*.xml ..\Binaries
+copy ..\Metadata\Sources\PeReader\bin\Release\Microsoft.Cci.PeReader.dll ..\Binaries
+copy ..\Metadata\Sources\PeReader\bin\Release\Microsoft.Cci.PeReader.pdb ..\Binaries
+copy ..\Metadata\Sources\PeReader\bin\Release\Microsoft.Cci.PeReader.xml ..\Binaries
 	
