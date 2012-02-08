ae9070384582a481c388c9729e78cfa79d0a2018
stobies_cp <SND\stobies_cp>
2012-02-08 01:29:14 -0800

    Make sure that name lookup caches in GlobalsClass are in sync with the members list

--- Sources/AstsProjectedAsCodeModel/Types.cs
+++ Sources/AstsProjectedAsCodeModel/Types.cs
@@ -380,7 +380,11 @@ namespace Microsoft.Cci.Ast {
           this.FillInWithGlobalFieldsAndMethods(members, nestedUnitNamespace);
         else {
           ITypeDefinitionMember/*?*/ typeMember = namespaceMember as ITypeDefinitionMember;
-          if (typeMember != null) members.Add(typeMember);
+          if (typeMember != null)
+          {
+            members.Add(typeMember);
+            this.AddMemberToCache(typeMember);
+          }
         }
       }
     }
